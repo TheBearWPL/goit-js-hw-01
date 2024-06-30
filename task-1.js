34308c573fbf0d5eb773e82532287d3fae56cd4c
TheBearWPL <maksymilian_jarosz@op.pl>
2024-06-30 22:41:02 +0200

    Update task-1.js

--- task-1.js
+++ task-1.js
@@ -1,1 +1,16 @@
 "use strict";
+
+const quantity = 5
+console.log(typeof quantity) = 5; // 5
+
+const pricePerDroid = 3000
+console.log(typeof pricePerDroid) = 3000; // 3000
+
+
+const quantity = 5
+const pricePerDroid = 3000
+console.log(quantity * pricePerDroid) = 3000; // 3000
+
+const message = 'You picked ${productName}, price per item is ${pricePerItem} credits' ;
+console.log(message);
+
