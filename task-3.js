7d41ea21bf4a98cd19896a943144ebefe21f159c
TheBearWPL <maksymilian_jarosz@op.pl>
2024-07-02 15:35:04 +0200

    Update task-3.js

--- task-3.js
+++ task-3.js
@@ -1,12 +1,14 @@
 "use strict";
 
 function getElementWidth(content, padding, border) {
-  const paddingValue = number.parseFloat(padding);
-  const contentValue = number.parseFloat(content);
-  const borderValue = number.parseFloat(border);
+  const paddingValue = parseFloat(padding);
+  const contentValue = parseFloat(content);
+  const borderValue = parseFloat(border);
 }
 
-const totalWidth = paddingValue + contentValue + borderValue;
+function (const totalWidth = paddingValue + contentValue + borderValue){;
+return totalWidth;
+}
 
 console.log(getElementWidth("50px", "8px", "4px")); // 74
 console.log(getElementWidth("60px", "12px", "8.5px")); // 101
